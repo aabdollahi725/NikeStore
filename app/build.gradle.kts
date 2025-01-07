d0e26db6064b5b192ace4d9b137788f106076506
Ahmadreza <aabdollahi725@gmail.com>
2025-01-07 11:58:30 +0330

    change fragments root view to coordinator layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,10 +9,10 @@ android {
 
     defaultConfig {
         applicationId = "com.example.nikestore"
-        minSdk = 22
+        minSdk = 21
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
