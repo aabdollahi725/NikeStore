b6908af7a3651121cd8c64346045f2e6d854e132
Ahmadreza Abdollahi <147329781+aabdollahi725@users.noreply.github.com>
2025-01-24 10:41:54 +0330

    add view binding to project

--- app/src/main/java/com/example/nikestore/feature/product/ProductDetailActivity.kt
+++ app/src/main/java/com/example/nikestore/feature/product/ProductDetailActivity.kt
@@ -6,16 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.nikestore.R
+import com.example.nikestore.databinding.ActivityProductDetailBinding
 
 class ProductDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProductDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_product_detail)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding=ActivityProductDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
